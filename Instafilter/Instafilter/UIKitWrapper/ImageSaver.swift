007a08ca60f1f632c735ef49ff3539979416b4fe
Anna <35114047+AKoneva@users.noreply.github.com>
2023-09-30 17:02:34 +0900

    Add saving picture

---
+++ Instafilter/Instafilter/UIKitWrapper/ImageSaver.swift
@@ -0,0 +1,18 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Анна Перехрест  on 2023/09/30.
+//
+
+import SwiftUI
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
