756c79fcb4e545804d53c60808d9ff89161c1c24
Anna <35114047+AKoneva@users.noreply.github.com>
2023-09-30 15:22:13 +0900

    init

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Анна Перехрест  on 2023/09/30.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
